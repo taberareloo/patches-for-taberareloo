862bd012b87a0fd6df70908196d1c0fcbc2d4c6f
YungSang <yungsang@gmail.com>
2013-03-21 15:46:57 -0700

    Add a sample model patch

---
+++ model.sample_patch.tbrl.js
@@ -0,0 +1,24 @@
+// ==Taberareloo==
+//{
+//  "name"        : "Sample Patch"
+//, "description" : "Sample patch for new model with minimal metadata"
+//, "include"     : ["background"]
+//}
+// ==/Taberareloo==
+
+(function() {
+  Models.register({
+    name       : 'Sample Model',
+    ICON       : 'http://yungsang.com/favicon.ico',
+    LINK       : 'http://yungsang.com/',
+    LOGIN_URL  : 'http://yungsang.com/',
+
+    check : function(ps) {
+      return /regular|photo|quote|link|video/.test(ps.type);
+    },
+
+    post : function(ps) {
+      return true;
+    }
+  });
+})();
