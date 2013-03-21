a2c5d993575d525c82355f79dfb60a5d2f0a3f3d
YungSang <yungsang@gmail.com>
2013-03-21 09:12:26 -0700

    Fix a bug

--- util.checkhttps.tbrl.js
+++ util.checkhttps.tbrl.js
@@ -5,7 +5,7 @@
 //, 'description' : 'Omit to check HTTPS'
 //, 'include'     : ['background', 'content']
 //, 'match'       : [/^https:\/\/.*/]
-//, 'version'     : 1.0
+//, 'version'     : 1.1
 //, 'downloadURL' : 'https://raw.github.com/YungSang/patches-for-taberareloo/master/util.checkhttps.tbrl.js'
 //, 'updateURL'   : 'https://raw.github.com/YungSang/patches-for-taberareloo/master/util.checkhttps.tbrl.js'
 //}
@@ -14,6 +14,8 @@
 (function() {
   addAround(window, 'checkHttps', function(proceed, args, target, methodName) {
     var ps = proceed(args);
+    ps.pageUrl = ps.https.pageUrl[1];
+    ps.itemUrl = ps.https.itemUrl[1];
     ps.https.pageUrl[0] = false;
     ps.https.itemUrl[0] = false;
     return ps;
