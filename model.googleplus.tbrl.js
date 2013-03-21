b1373bcc495624b9054c2ae8a2660df9417e3484
YungSang <yungsang@gmail.com>
2013-03-21 10:01:13 -0700

    Fix a bug with JSON.parse

--- model.googleplus.tbrl.js
+++ model.googleplus.tbrl.js
@@ -1,12 +1,12 @@
 // ==Taberareloo==
 //{
-//  'name'        : 'New Google+ Icon'
-//, 'namespace'   : 'https://github.com/YungSang/patches-for-taberareloo'
-//, 'description' : 'Use new Google+ icon'
-//, 'include'     : ['background']
-//, 'version'     : 1.0
-//, 'downloadURL' : 'https://raw.github.com/YungSang/patches-for-taberareloo/master/model.googleplus.tbrl.js'
-//, 'updateURL'   : 'https://raw.github.com/YungSang/patches-for-taberareloo/master/model.googleplus.tbrl.js'
+//  "name"        : "New Google+ Icon"
+//, "namespace"   : "https://github.com/YungSang/patches-for-taberareloo"
+//, "description" : "Use new Google+ icon"
+//, "include"     : ["background"]
+//, "version"     : 1.0
+//, "downloadURL" : "https://raw.github.com/YungSang/patches-for-taberareloo/master/model.googleplus.tbrl.js"
+//, "updateURL"   : "https://raw.github.com/YungSang/patches-for-taberareloo/master/model.googleplus.tbrl.js"
 //}
 // ==/Taberareloo==
 
--- util.checkhttps.tbrl.js
+++ util.checkhttps.tbrl.js
@@ -1,13 +1,13 @@
 // ==Taberareloo==
 //{
-//  'name'        : 'No Check HTTPS'
-//, 'namespace'   : 'https://github.com/YungSang/patches-for-taberareloo'
-//, 'description' : 'Omit to check HTTPS'
-//, 'include'     : ['background', 'content']
-//, 'match'       : [/^https:\/\/.*/]
-//, 'version'     : 1.1
-//, 'downloadURL' : 'https://raw.github.com/YungSang/patches-for-taberareloo/master/util.checkhttps.tbrl.js'
-//, 'updateURL'   : 'https://raw.github.com/YungSang/patches-for-taberareloo/master/util.checkhttps.tbrl.js'
+//  "name"        : "No Check HTTPS"
+//, "namespace"   : "https://github.com/YungSang/patches-for-taberareloo"
+//, "description" : "Omit to check HTTPS"
+//, "include"     : ["background", "content"]
+//, "match"       : ["^https:\/\/.*"]
+//, "version"     : 1.2
+//, "downloadURL" : "https://raw.github.com/YungSang/patches-for-taberareloo/master/util.checkhttps.tbrl.js"
+//, "updateURL"   : "https://raw.github.com/YungSang/patches-for-taberareloo/master/util.checkhttps.tbrl.js"
 //}
 // ==/Taberareloo==
 
