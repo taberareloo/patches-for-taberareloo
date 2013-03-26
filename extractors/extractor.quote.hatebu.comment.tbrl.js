11f2aca6a2ef942dc01678633583751b079e49ef
YungSang <yungsang@gmail.com>
2013-03-25 18:43:52 -0700

    Fix a bug with a keyboard shortcut

--- extractors/extractor.quote.hatebu.comment.tbrl.js
+++ extractors/extractor.quote.hatebu.comment.tbrl.js
@@ -1,10 +1,10 @@
 // ==Taberareloo==
 // {
 //   "name"        : "Quote Extractor for Hatebu Comment"
-// , "description" : "Post to twittaw.com"
+// , "description" : "Extract a hatebu comment"
 // , "include"     : ["content"]
 // , "match"       : ["http://b.hatena.ne.jp/entry/*"]
-// , "version"     : "1.0"
+// , "version"     : "1.2.1"
 // , "downloadURL" : "https://raw.github.com/YungSang/patches-for-taberareloo/master/extractors/extractor.quote.hatebu.comment.tbrl.js"
 // }
 // ==/Taberareloo==
@@ -20,15 +20,14 @@
     },
 
     extract : function(ctx) {
-      var node      = this.getComment(ctx);
+      var node      = this.saved_node;
       var link      = $X('.//a[@class="username"]', node)[0];
       var username  = node.id.replace(/bookmark-user-([A-Za-z0-9_-]{3,15})/, '$1');
       var selection = createFlavoredString($X('.//span[@class="comment"]', node)[0]);
-
       if (ctx.selection) {
         selection = ctx.selection;
       }
-      var ps = {
+      return {
         type    : 'quote',
         item    : ctx.title.replace(/-/, '- ' + username + ' -'),
         itemUrl : link.href,
@@ -37,15 +36,17 @@
           html  : selection.html
         }
       };
-      return ps;
     },
+
+    saved_node : null,
 
     getComment : function(ctx) {
       var node = ctx.target;
       if (ctx.selection) {
         node = ctx.window.getSelection().anchorNode;
       }
-      return $X('./ancestor-or-self::li[starts-with(@id, "bookmark-user-")]', node)[0];
+      this.saved_node = $X('./ancestor-or-self::li[starts-with(@id, "bookmark-user-")]', node)[0];
+      return this.saved_node;
     }
   }, 'Quote');
 })();
