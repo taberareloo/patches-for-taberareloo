b608d4b02cb6edbce6ce61211c831bcf437e3824
YungSang <yungsang@gmail.com>
2013-03-26 07:11:32 -0700

    Add 'Photo - Twitter Dashboard'

--- extractors/extractor.quote.twitter.dashboard.tbrl.js
+++ extractors/extractor.quote.twitter.dashboard.tbrl.js
@@ -4,13 +4,56 @@
 // , "description" : "Extract a tweet on a dashboard"
 // , "include"     : ["content"]
 // , "match"       : ["*://twitter.com/*"]
-// , "version"     : "1.0"
+// , "version"     : "1.1"
 // , "downloadURL" : "https://raw.github.com/YungSang/patches-for-taberareloo/master/extractors/extractor.quote.twitter.dashboard.tbrl.js"
 // }
 // ==/Taberareloo==
 
 (function() {
-  Extractors.register({
+  Extractors.register([
+  {
+    name : 'Photo - Twitter Dashboard',
+
+    check : function(ctx) {
+      return ctx.onImage && !!this.getTweet(ctx);
+    },
+
+    extract : function(ctx) {
+      var node      = this.saved_node;
+      var link      = $X('.//a[contains(concat(" ",@class," ")," details ")]', node)[0];
+      var username  = $X('.//strong[contains(concat(" ",@class," ")," fullname ")]/text()', node)[0];
+      var selection = createFlavoredString($X('.//p[@class="js-tweet-text"]', node)[0]);
+      if (ctx.selection) {
+        var node2 = ctx.window.getSelection().anchorNode;
+        node2 = $X('./ancestor-or-self::li[starts-with(@id, "stream-item-tweet-")]', node2)[0];
+        if (node == node2) {
+          selection = ctx.selection;
+        }
+      }
+      return {
+        type     : 'photo',
+        item     : 'Twitter / ' + username,
+        itemUrl  : ctx.target.src,
+        body     : selection.raw,
+        flavors  : {
+          html : selection.html
+        },
+        favorite : {
+          name : 'Twitter',
+          id   : link.href.match(/(status|statuses)\/(\d+)/)[2]
+        }
+      };
+    },
+
+    saved_node : null,
+
+    getTweet : function(ctx) {
+      var node = ctx.target;
+      this.saved_node = $X('./ancestor-or-self::li[starts-with(@id, "stream-item-tweet-")]', node)[0];
+      return this.saved_node;
+    }
+  },
+  {
     name : 'Quote - Twitter Dashboard',
 
     check : function(ctx) {
@@ -26,12 +69,16 @@
         selection = ctx.selection;
       }
       return {
-        type    : 'quote',
-        item    : 'Twitter / ' + username,
-        itemUrl : link.href,
-        body    : selection.raw,
-        flavors : {
-          html  : selection.html
+        type     : 'quote',
+        item     : 'Twitter / ' + username,
+        itemUrl  : link.href,
+        body     : selection.raw,
+        flavors  : {
+          html : selection.html
+        },
+        favorite : {
+          name : 'Twitter',
+          id   : link.href.match(/(status|statuses)\/(\d+)/)[2]
         }
       };
     },
@@ -46,5 +93,5 @@
       this.saved_node = $X('./ancestor-or-self::li[starts-with(@id, "stream-item-tweet-")]', node)[0];
       return this.saved_node;
     }
-  }, 'Quote - Twitter', true);
+  }], 'Quote - Twitter', true);
 })();
