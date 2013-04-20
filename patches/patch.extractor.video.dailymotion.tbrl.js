536304d2c7b4026557f18686db041fd228d6cc81
YungSang <yungsang@gmail.com>
2013-04-20 13:53:14 -0700

    Revise a video size of dailymotion Will merge into Taberareloo

--- patches/patch.extractor.video.dailymotion.tbrl.js
+++ patches/patch.extractor.video.dailymotion.tbrl.js
@@ -4,7 +4,7 @@
 // , "description" : "Extract a Dailymotion video"
 // , "include"     : ["content"]
 // , "match"       : ["http://www.dailymotion.com/video/*"]
-// , "version"     : "1.0.0"
+// , "version"     : "1.1.0"
 // , "downloadURL" : "https://raw.github.com/YungSang/patches-for-taberareloo/master/patches/patch.extractor.video.dailymotion.tbrl.js"
 // }
 // ==/Taberareloo==
@@ -16,12 +16,17 @@
     },
     extract : function(ctx) {
       var player = this.getPlayer(ctx);
+      var width  = $X('//meta[@name="twitter:player:width"]/@content', ctx.document)[0];
+      var height = $X('//meta[@name="twitter:player:height"]/@content', ctx.document)[0];
+
+      ctx.title = $X('//meta[@property="og:title"]/@content', ctx.document)[0];
+      ctx.href  = $X('//meta[@property="og:url"]/@content', ctx.document)[0];
 
       return {
-        type      : 'video',
-        item      : $X('//meta[@property="og:title"]/@content', ctx.document)[0],
-        itemUrl   : $X('//meta[@property="og:url"]/@content', ctx.document)[0],
-        body      : '<iframe frameborder="0" width="480" height="270" src="' + player + '"></iframe>'
+        type    : 'video',
+        item    : ctx.title,
+        itemUrl : ctx.href,
+        body    : '<iframe frameborder="0" width="'+width+'" height="'+height+'" src="'+player+'"></iframe>'
       };
     },
     getPlayer : function(ctx) {
