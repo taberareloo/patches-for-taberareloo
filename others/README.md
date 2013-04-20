ad67ee4a1dad9e17a69c66b94390f0222cc54c26
YungSang <yungsang@gmail.com>
2013-04-19 22:49:49 -0700

    Add URL Generator for Gunosy RSS

--- others/README.md
+++ others/README.md
@@ -4,6 +4,9 @@
 
 ## パッチ一覧
 
+* [menu.gunosy.rss.tbrl.js](https://raw.github.com/YungSang/patches-for-taberareloo/master/others/menu.gunosy.rss.tbrl.js)  
+	[Gunosy](http://gunosy.com) の RSS フィード生成 Web サービス [Gunosy RSS](http://dai-shi.github.io/gunosy-rss/) の上級者向け RSS リンク(学習可能バージョン)を自動生成するツール
+
 * [menu.photo.search.ascii2d.tbrl.js](https://raw.github.com/YungSang/patches-for-taberareloo/master/others/menu.photo.search.ascii2d.tbrl.js)  
 	選択した画像を [二次元画像詳細検索](http://www.ascii2d.net/imagesearch) で検索するパッチ
 
---
+++ others/menu.gunosy.rss.tbrl.js
@@ -0,0 +1,56 @@
+// ==Taberareloo==
+// {
+//   "name"        : "Gunosy RSS"
+// , "description" : "URL Generator for Gunosy RSS"
+// , "include"     : ["background"]
+// , "version"     : "0.1.0"
+// , "downloadURL" : "https://raw.github.com/YungSang/patches-for-taberareloo/master/others/menu.gunosy.rss.tbrl.js"
+// }
+// ==/Taberareloo==
+
+(function() {
+  var NAME      = 'Gunosy';
+  var BASE_URL  = 'http://gunosy-rss.herokuapp.com/';
+
+  Menus._register({
+    type     : 'separator',
+    contexts : ['all']
+  });
+  Menus._register({
+    title    : 'Generate URL for Gunosy RSS',
+    contexts : ['all'],
+    onclick  : function(info, tab) {
+      request('http://gunosy.com/edit/profile').addCallback(function(res) {
+        var doc = createHTML(res.responseText);
+ 
+        var user_name = $X('id("usre_name")/@value', doc)[0];
+        if (!user_name) {
+          return alert(chrome.i18n.getMessage('error_notLoggedin', NAME));
+        }
+
+        var user_id = $X('//form[@class="edit_user"]/@id', doc)[0];
+        user_id = user_id.substring(10);
+
+        getCookies('gunosy.com', '_gunosy_session').addCallback(function(cookies) {
+          if (!cookies.length) return alert(chrome.i18n.getMessage('error_notLoggedin', NAME));
+
+          var url = BASE_URL + user_name + '.rss' + queryString({
+            u              : user_id,
+            gunosy_session : _gunosy_session = cookies[0].value
+          }, true);
+
+          var textarea = $N('textarea');
+          document.body.appendChild(textarea);
+          textarea.value = url;
+          textarea.select();
+          document.execCommand('copy');
+          document.body.removeChild(textarea);
+
+          alert(url + "\nをクリップボードにコピーしました");
+        });
+      });
+    }
+  });
+
+  Menus.create();
+})();
