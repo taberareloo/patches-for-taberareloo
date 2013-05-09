b5681d7e4027014c5862b3bc2fe539659609aa86
YungSang <yungsang@gmail.com>
2013-05-09 14:21:37 -0700

    Omit the case of reshare

--- patches/patch.model.googleplus.quote.tbrl.js
+++ patches/patch.model.googleplus.quote.tbrl.js
@@ -4,7 +4,7 @@
 // , "namespace"   : "https://github.com/YungSang/patches-for-taberareloo"
 // , "description" : "Move a long selection from body to description"
 // , "include"     : ["background"]
-// , "version"     : "1.2.0"
+// , "version"     : "1.2.1"
 // , "downloadURL" : "https://raw.github.com/YungSang/patches-for-taberareloo/master/patches/patch.model.googleplus.quote.tbrl.js"
 // }
 // ==/Taberareloo==
@@ -12,7 +12,7 @@
 (function() {
   addAround(Models['Google+'], 'post', function(proceed, args, target, methodName) {
     var ps = update({}, args[0]);
-    if (ps.body) {
+    if (!ps.reshare && ps.body) {
       var body = ps.body.trimTag();
       if (body.length > 200) {
         ps.description = joinText([ps.description, '“' + body + '”'], "\n\n");
