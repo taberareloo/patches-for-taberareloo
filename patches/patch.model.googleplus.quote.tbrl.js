8ff9a00716d109d3c41e3fa376ae5f38522ef4b0
YungSang <yungsang@gmail.com>
2013-05-12 13:39:11 -0700

    Replace br to nl

--- patches/patch.model.googleplus.quote.tbrl.js
+++ patches/patch.model.googleplus.quote.tbrl.js
@@ -4,7 +4,7 @@
 // , "namespace"   : "https://github.com/YungSang/patches-for-taberareloo"
 // , "description" : "Move a long selection from body to description"
 // , "include"     : ["background"]
-// , "version"     : "1.2.2"
+// , "version"     : "1.2.3"
 // , "downloadURL" : "https://raw.github.com/YungSang/patches-for-taberareloo/master/patches/patch.model.googleplus.quote.tbrl.js"
 // }
 // ==/Taberareloo==
@@ -13,7 +13,8 @@
   addAround(Models['Google+'], 'post', function(proceed, args, target, methodName) {
     var ps = update({}, args[0]);
     if (!ps.reshare && ps.body) {
-      var body = ps.body.trimTag().trim();
+      var body = ps.body.replace(/<br(\s*\/)?>/ig, "\n");
+      body = body.trimTag().trim();
       if (body.length > 200) {
         ps.description = joinText([ps.description, '“' + body + '”'], "\n\n");
         ps.body = null;
