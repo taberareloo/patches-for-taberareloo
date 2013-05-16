5ba57f67e40f9e0c562ffeac0e990fc01b0a6b0f
YungSang <yungsang@gmail.com>
2013-05-16 14:46:55 -0700

    Move a selection from body to description always

--- patches/patch.model.googleplus.quote.tbrl.js
+++ patches/patch.model.googleplus.quote.tbrl.js
@@ -2,9 +2,9 @@
 // {
 //   "name"        : "Optimize Quote Post to Google+"
 // , "namespace"   : "https://github.com/YungSang/patches-for-taberareloo"
-// , "description" : "Move a long selection from body to description"
+// , "description" : "Move a selection from body to description"
 // , "include"     : ["background"]
-// , "version"     : "1.2.3"
+// , "version"     : "1.2.4"
 // , "downloadURL" : "https://raw.github.com/YungSang/patches-for-taberareloo/master/patches/patch.model.googleplus.quote.tbrl.js"
 // }
 // ==/Taberareloo==
@@ -13,12 +13,13 @@
   addAround(Models['Google+'], 'post', function(proceed, args, target, methodName) {
     var ps = update({}, args[0]);
     if (!ps.reshare && ps.body) {
-      var body = ps.body.replace(/<br(\s*\/)?>/ig, "\n");
+      var body = ps.body.replace(/\r\n/g, "\n");
+      body = body.replace(/\n<br(\s*\/)?>/ig, "\n");
+      body = body.replace(/<br(\s*\/)?>\n/ig, "\n");
+      body = body.replace(/<br(\s*\/)?>/ig, "\n");
       body = body.trimTag().trim();
-      if (body.length > 200) {
-        ps.description = joinText([ps.description, '“' + body + '”'], "\n\n");
-        ps.body = null;
-      }
+      ps.description = joinText([ps.description, '“' + body + '”'], "\n\n");
+      ps.body = null;
     }
     return proceed([ps]);
   });
