a19b9f963d8e675ff48293e171f3be5a732c65d3
YungSang <yungsang@gmail.com>
2013-05-17 23:50:24 -0700

    Add no_alert parameter at Patches.install

--- others/menu.backup.config.tbrl.js
+++ others/menu.backup.config.tbrl.js
@@ -3,12 +3,90 @@
 //   "name"        : "Backup/Restore Configurations"
 // , "description" : "Backup/Restore Configurations using data URI"
 // , "include"     : ["background"]
-// , "version"     : "0.2.2"
+// , "version"     : "0.3.0"
 // , "downloadURL" : "https://raw.github.com/YungSang/patches-for-taberareloo/master/others/menu.backup.config.tbrl.js"
 // }
 // ==/Taberareloo==
 
 (function() {
+  update(Patches, {
+    install : function (file, no_alert) {
+      var self = this;
+
+      function save(fileName, file, url) {
+        switch (file.type) {
+        case 'text/plain':
+        case 'text/javascript':
+        case 'application/javascript':
+        case 'application/x-javascript':
+          break;
+        default:
+          return succeed();
+        }
+
+        if (!file.size) {
+          return succeed();
+        }
+
+        var deferred = new Deferred();
+
+        self.getMetadata(file).addCallback(function (metadata) {
+          if (metadata) {
+            self.dirEntry.getFile(fileName, { create: true },
+              function (fileEntry) {
+                fileEntry.createWriter(
+                  function (fileWriter) {
+                    fileWriter.onwriteend = function () {
+                      this.onwriteend = null;
+                      this.truncate(this.position);
+                      self.loadAndRegister(fileEntry, metadata, url).addCallback(function (patch) {
+                        console.log('Install patch: ' + fileEntry.fullPath);
+                        if (!no_alert) {
+                          alert(chrome.i18n.getMessage('message_installed', fileName));
+                        }
+                        deferred.callback(patch);
+                      });
+                    };
+                    fileWriter.onerror = function (e) {
+                      deferred.errback(e);
+                    };
+                    fileWriter.write(file);
+                  },
+                  function (e) {
+                    deferred.errback(e);
+                  }
+                );
+              },
+              function (e) {
+                deferred.errback(e);
+              }
+            );
+          } else {
+            deferred.errback();
+          }
+        });
+
+        return deferred;
+      }
+
+      if (typeof file === 'string') {
+        var url      = file;
+        var fileName = url.replace(/\\/g, '/').replace(/.*\//, '');
+        return request(url + '?_=' + (new Date()).getTime(), {
+          responseType: 'blob'
+        }).addCallback(function (res) {
+          return save(fileName, res.response, url).addCallback(function (patch) {
+            return !!patch;
+          });
+        });
+      } else {
+        return save(file.name, file).addCallback(function (patch) {
+          return !!patch;
+        });
+      }
+    }
+  });
+
   var DATA_TYPE = 'text/plain;charset=utf-8;name=taberareloo.json';
 
   Menus._register({
