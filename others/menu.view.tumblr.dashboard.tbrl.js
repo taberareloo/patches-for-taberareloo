c070d22b258d978598313522a89437e201d4e837
YungSang <yungsang@gmail.com>
2013-12-19 09:54:23 -0800

    Revise the url based on taizooo's script

--- others/menu.view.tumblr.dashboard.tbrl.js
+++ others/menu.view.tumblr.dashboard.tbrl.js
@@ -3,21 +3,21 @@
 //   "name"        : "View Tumblr Dashboard"
 // , "description" : "View Tumblr Dashboard page with the post"
 // , "include"     : ["background"]
-// , "version"     : "0.1.0"
+// , "version"     : "0.2.0"
 // , "downloadURL" : "https://raw.github.com/YungSang/patches-for-taberareloo/master/others/menu.view.tumblr.dashboard.tbrl.js"
 // }
 // ==/Taberareloo==
 
 // https://github.com/polygonplanet/tombloo/blob/master/tombloo.model.tumblr.view.dashboard.by.permalink.js
+// http://let.hatelabo.jp/taizooo/let/gYC-xpPJiqHJGQ
 
 (function() {
-
   var tumblrSinglePageUrl = [
     '*://*.tumblr.com/post/*'
   ];
   Menus._register({
     type     : 'separator',
-    contexts : ['page'],
+    contexts : ['all'],
     documentUrlPatterns : tumblrSinglePageUrl
   });
   Menus._register({
@@ -27,10 +27,9 @@
     onclick  : function(info, tab) {
       var post_id = info.pageUrl.extract(/tumblr\.com\/post\/([0-9]+)/);
       chrome.tabs.create({
-        url : 'http://www.tumblr.com/dashboard/999/' + (parseInt(post_id, 10) + 1)
+        url : 'http://www.tumblr.com/dashboard/999/?offset=' + (parseInt(post_id, 10) + 1)
       });
     }
   });
-
   Menus.create();
 })();
