b509e4046b00199cf64e3be9e4b158b13f6a95bc
YungSang <yungsang@gmail.com>
2013-12-18 20:57:42 -0800

    Fix a bug with invalid URL

--- models/model.googl.tbrl.js
+++ models/model.googl.tbrl.js
@@ -3,7 +3,7 @@
 //   "name"        : "Goo.gl Model"
 // , "description" : "Add Google URL Shortener for Twitter"
 // , "include"     : ["background"]
-// , "version"     : "1.0.0"
+// , "version"     : "1.0.1"
 // , "downloadURL" : "https://raw.github.com/YungSang/patches-for-taberareloo/master/models/model.googl.tbrl.js"
 // }
 // ==/Taberareloo==
@@ -17,7 +17,7 @@
     HOME_URL : 'http://goo.gl/',
     POST_URL : 'http://goo.gl/api/shorten',
 
-    getSecurityToken : function() {
+    getSecurityToken : function () {
       return request(this.HOME_URL, { responseType: 'document' }).addCallback(function(res) {
         var doc = res.response;
         var security_token = $X('//input[@name="security_token"]/@value', doc)[0];
@@ -28,21 +28,23 @@
       });
     },
 
-    shorten : function(url) {
+    shorten : function (url) {
       var self = this;
       if(/\/\/goo\.gl\//.test(url)) {
         return succeed(url);
       }
 
-      return this.getSecurityToken().addCallback(function(security_token) {
+      return this.getSecurityToken().addCallback(function (security_token) {
         return request(self.POST_URL, {
           sendContent : {
             url            : url,
             security_token : security_token
           }
-        }).addCallback(function(res) {
+        }).addCallback(function (res) {
           var json = JSON.parse(res.responseText);
           return json.short_url;
+        }).addErrback(function (res) {
+          return '';
         });
       });
     }
